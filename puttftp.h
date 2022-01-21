cb24e5cc1a5c0a759035c0c90656d93fa8ccac8a
theogillet <theo.gillet@ensea.fr>
2022-01-21 04:31:11 +0100

    Version1

---
+++ puttftp.h
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <fcntl.h>
+
+#define BUFSIZE 512
+#define BUFTAILLE 100
